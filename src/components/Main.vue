df95d79805c4eeeda24e67503c79f235f5ea4aff
Sarsenovz2z <nurik9293709@gmail.com>
2018-11-24 00:49:36 +0600

    front

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,60 +10,52 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="top">
-                            <h4><center><i>ОАРИТ</i></center></h4>
+                            <h5><center><i>ОАРИТ</i></center></h5>
                             <div class="table-left">
                                 <table class="table table-bordered">
                                     <tbody>
                                         <tr>
-                                            <td colspan="2" v-contenteditable:testText="true"></td>
-                                            <td v-contenteditable:testText="true"></td>
+                                            <td colspan="2" class="input-hidden" v-contenteditable:date="true"></td>
+                                            <td class="input-hidden" v-contenteditable:time.dangerousHTML="true"></td>
                                         </tr>
                                         <tr>
                                             <td>t-ра</td>
-                                            <td v-contenteditable:testText="true"></td>
-                                            <td>0С</td>
+                                            <td class="input-hidden" v-contenteditable:temperature="true"></td>
+                                            <td><sup><small>0</small></sup>С</td>
                                         </tr>
                                         <tr>
-                                            <td>SpO2</td>
-                                            <td v-contenteditable:testText="true"></td>
+                                            <td>SpO<sub><small>2</small></sub></td>
+                                            <td class="input-hidden" v-contenteditable:spo="true"></td>
                                             <td>%</td>
                                         </tr>
                                         <tr>
                                             <td>АД</td>
-                                            <td v-contenteditable:testText="true"></td>
+                                            <td class="input-hidden" v-contenteditable:ad="true"></td>
                                             <td>мм.рт.ст.</td>
                                         </tr>
                                         <tr>
                                             <td>ЧСС</td>
-                                            <td v-contenteditable:testText="true"></td>
+                                            <td class="input-hidden" v-contenteditable:chss="true"></td>
                                             <td>в мин</td>
                                         </tr>
                                         <tr>
                                             <td>ЦВД</td>
-                                            <td v-contenteditable:testText="true"></td>
+                                            <td class="input-hidden" v-contenteditable:svd="true"></td>
                                             <td>мм.рт.ст.</td>
                                         </tr>
                                     </tbody>
                                 </table>
                             </div>
-                            <p class="content" v-contenteditable:testText="true">
+                            <p class="content">
+                                <span class="input-hidden" v-contenteditable:content="true"></span>
                             </p>
                         </div>
                     </div>
                     <div class="col-md-12">
-                        <div class="bottom">
-                            <h4>Предполагаемая тактика:</h4>
-                            <ul class="tactic" contenteditable="true">
-                                <li>Антибактериальная терапия, Гастропротекция</li>
-                                <li>Контроль витальных функций, коррекция нарушений ВЭБ</li>
-                                <li>Адекватная анальгезия, Симптоматическая терапия</li>
-                                <li>Динамическое наблюдение, КЩС, БАК, АЧТВ, коагулограмма</li>
-                                <li>ЭКГ, рентгенограмма органов грудной клетки</li>
-                                <li>
-                                    При стабильно-положительной динамике
-                                    - экстубация, перевод в профильное отделение
-                                </li>
-                            </ul>
+                        <div class="bottom" v-show="tactic.length > 0">
+                            <h5>Предполагаемая тактика:</h5>
+                            <p class="tactic input-hidden" v-contenteditable:tactic="true">
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -82,60 +74,59 @@ export default {
             transcription: [],
             language: 'ru-RU',
             isActive: false,
-            testText: 'Test Text',
-            parsed: {
-
-            },
+            content: 'Говорите ...',
+            tactic:'',
+            date: '29.10.2018',
+            time: '13<sup><small>05</small></sup>',
+            temperature: '',
+            spo: '',
+            ad: '',
+            chss: '',
+            svd: '',
         }
     },
     methods: {
-        start: function(e) {
+        start: function (e) {
             // this.activate()
-            $('#main').fadeOut('slow')
+            $('#main').fadeOut('slow');
         },
-        test: function(e) {
-            console.log(this.testText)
+        test: function (e) {
+            console.log(this.testText);
         },
-        update: function(text) {
+        update: function (text) {
             // Google API text receive Event
-            console.log(text)
+            console.log(text);
         },
-        checkApi () {
-            window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-            if (!SpeechRecognition && process.env.NODE_ENV !== 'production') {
-                throw new Error('Speech Recognition does not exist on this browser. Use Chrome or Firefox')
-            }
-            if (!SpeechRecognition) {
-                return
-            }
-            this.recognition = new SpeechRecognition()
-            this.recognition.lang = this.language
-            this.recognition.interimResults = true
-            this.recognition.addEventListener('result', event => {
+        checkApi: function () {
+            window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            this.recognition = new SpeechRecognition();
+            this.recognition.lang = this.language;
+            this.recognition.interimResults = true;
+            this.recognition.addEventListener('result', (event) => {
                 const text = Array.from(event.results)
                 .map(result => result[0])
                 .map(result => result.transcript)
-                .join('')
-                this.runtimeTranscription = text
-            })
+                .join('');
+                this.runtimeTranscription = text;
+            });
             this.recognition.addEventListener('end', () => {
                 if (this.runtimeTranscription !== '') {
-                    this.transcription.push(this.runtimeTranscription)
-                    this.update(this.transcription)
+                    this.transcription.push(this.runtimeTranscription);
+                    this.update(this.transcription);
                 }
-                this.runtimeTranscription = ''
-                this.recognition.start()
-            })
-            this.recognition.start()
+                this.runtimeTranscription = '';
+                this.recognition.start();
+            });
+            this.recognition.start();
         },
         activate() {
             if (!this.isActive) {
-                this.checkApi()
-                this.isActive = true
+                this.checkApi();
+                this.isActive = true;
             } else {
-                this.isActive = false
-                this.recognition.stop()
-                this.recognition = null
+                this.isActive = false;
+                this.recognition.stop();
+                this.recognition = null;
             }
         },
     },
